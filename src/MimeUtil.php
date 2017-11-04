b3d3362c1a73251649898d79007bad14f7e815eb
Christian Gripp <mail@core23.de>
2017-11-04 22:40:25 +0000

    Apply fixes from StyleCI

--- src/MimeUtil.php
+++ src/MimeUtil.php
@@ -941,7 +941,7 @@ final class MimeUtil
      */
     public static function getInstance(): self
     {
-        if (self::$instance === null) {
+        if (null === self::$instance) {
             self::$instance = new self();
         }
 
@@ -969,7 +969,7 @@ final class MimeUtil
      *
      * @return string|null
      */
-    public function getTypeFromFilename(string $filename) : ? string
+    public function getTypeFromFilename(string $filename): ? string
     {
         return $this->getTypeFromExtension(pathinfo($filename, PATHINFO_EXTENSION));
     }
@@ -981,7 +981,7 @@ final class MimeUtil
      *
      * @return string[]
      */
-    public function getExtensionsFromType(string $mimeType) : array
+    public function getExtensionsFromType(string $mimeType): array
     {
         $mimeType = strtolower($mimeType);
 
